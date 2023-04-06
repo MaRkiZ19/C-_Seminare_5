6e69bd78b67fce745af399528289e4572267810e
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-06 22:20:23 +0300

    task04

---
+++ task04/Program.cs
@@ -0,0 +1,24 @@
+﻿// расчёт интеграла    y = k * x + b
+
+using System;
+DateTime now = DateTime.Now;
+
+int k = 5;
+int b = 4;
+
+int x0 = 0;
+int xLast = 1500;
+double offset = 0.00001;
+
+double result = 0;
+int count = 0;
+for (double i = x0; i < xLast; i+= offset)
+{
+    double rectangleSquare = ((offset + i) * k + b) * offset;
+    result += rectangleSquare;
+    count ++;
+}
+DateTime end = DateTime.Now;
+System.Console.WriteLine(result);
+System.Console.WriteLine(count);
+System.Console.WriteLine(end - now);
